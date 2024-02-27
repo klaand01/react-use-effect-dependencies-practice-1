68b9ebc75591c82f09a2c5f32cf3d02a2ddc1112
Klara Andersson <klara.andersson@se.experis.com>
2024-02-27 10:42:25 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
-  const [dataType, setDataType] = useState("");
+  const [dataType, setDataType] = useState("people");
 
   const [data, setData] = useState(null);
 
   console.log({ data });
 
   // Write code here.
-  //
+  useEffect(() => 
+  {
+    console.log("CHANGED");
+    fetch(`https://swapi.dev/api/${dataType}/`)
+    .then((response) => response.json())
+    .then((data) => setData(data))
+  }, [dataType])
 
   return (
     <div>
